63699fba47c5f958a46957520d5217f2f593c014
Igor Narazin <in@isfb.ru>
2014-07-31 19:22:15 +0400

    bill address def

--- app/controllers/spree/customer_details_controller_decorator.rb
+++ app/controllers/spree/customer_details_controller_decorator.rb
@@ -2,8 +2,7 @@ Spree::Admin::Orders::CustomerDetailsController.class_eval do
         before_filter :default_params
 
         def default_params
-          stock = Spree::StockLocation.first
-          @order.email ||= 'empty@empty.ru'
+          @order.email ||= ''
           @order.bill_address ||= Spree::Address.default(try_spree_current_user, "bill")
         end
 
