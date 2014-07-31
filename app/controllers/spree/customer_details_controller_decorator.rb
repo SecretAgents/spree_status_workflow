42bcfeb12263c7e69950f9a90b72d156a4eddef3
Igor Narazin <in@isfb.ru>
2014-07-31 19:18:11 +0400

    bill address def

--- app/controllers/spree/customer_details_controller_decorator.rb
+++ app/controllers/spree/customer_details_controller_decorator.rb
@@ -4,16 +4,7 @@ Spree::Admin::Orders::CustomerDetailsController.class_eval do
         def default_params
           stock = Spree::StockLocation.first
           @order.email ||= 'empty@empty.ru'
-          @order.bill_address ||= Spree::Address.create({
-                                                           address1: '-',
-                                                           city: '-',
-                                                           phone: 7,
-                                                           zipcode: 000,
-                                                           country: Spree::Country.find_by_name!("Russian Federation"),
-                                                           state_name: stock.name,
-                                                           state_id: stock.id
-                                                       })
-
+          @order.bill_address ||= Spree::Address.default(try_spree_current_user, "bill")
         end
 
 end
